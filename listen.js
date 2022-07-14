39aa8d92b9df531faa2112a7647750e467fdd64b
Ian Findlay <code@findlayian.com>
2022-07-14 21:41:35 +0100

    Update listen.js to work with Heroku

--- listen.js
+++ listen.js
@@ -1,3 +1,5 @@
 const app = require("./app");
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
