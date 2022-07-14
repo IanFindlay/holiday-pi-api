946c0f06e04703ef2088cb5accf1a2fffc6f0e6a
Ian Findlay <code@findlayian.com>
2022-07-14 21:36:08 +0100

    Refactor GET /airports test

--- test/app.test.js
+++ test/app.test.js
@@ -4,31 +4,6 @@ const request = require("supertest");
 const app = require("../app");
 jest.mock("axios");
 
-const airportOne = {
-  id: "AP1",
-  name: "airport 1",
-  latitude: "1",
-  longitude: "1",
-  connections: [{ id: "AP2", miles: 20 }],
-};
-const airportTwo = {
-  id: "AP2",
-  name: "airport 2",
-  latitude: "2",
-  longitude: "2",
-  connections: [
-    { id: "AP1", miles: 20 },
-    { id: "AP3", miles: 10 },
-  ],
-};
-const airportThree = {
-  id: "AP3",
-  name: "airport 3",
-  latitude: "3",
-  longitude: "3",
-  connections: [{ id: "AP1", miles: 5 }],
-};
-
 describe("app", () => {
   describe("Invalid endpoint", () => {
     it("should have status 404 and respond with a msg of 'Path not found' when an incorrect endpoint is requested", () => {
@@ -42,10 +17,36 @@ describe("app", () => {
   });
 
   describe("GET /api/airports", () => {
+    const airports = [
+      {
+        id: "AP1",
+        name: "airport 1",
+        latitude: "1",
+        longitude: "1",
+        connections: [{ id: "AP2", miles: 20 }],
+      },
+      {
+        id: "AP2",
+        name: "airport 2",
+        latitude: "2",
+        longitude: "2",
+        connections: [
+          { id: "AP1", miles: 20 },
+          { id: "AP3", miles: 10 },
+        ],
+      },
+      {
+        id: "AP3",
+        name: "airport 3",
+        latitude: "3",
+        longitude: "3",
+        connections: [{ id: "AP1", miles: 5 }],
+      },
+    ];
     it(`should have a status of 200 and return an object with a key of airports and a value of an array of airport objects
         with id, name, latitude and longitude key value pairs`, () => {
       axios.get.mockResolvedValue({
-        data: [airportOne, airportTwo, airportThree],
+        data: airports,
       });
 
       return request(app)
